560cdf7e68068bbe4e50a8faf9eaccb2b9f09ca7
EnzoAyala <rickayalatarazona@gmail.com>
2025-09-15 18:22:29 -0500

    Creacion de la base de datos y archivos de login y register (faltan completar)

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 from flask import Flask, render_template
+from config import Config
+from models import db, seed_data
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-@app.route('/catalogo')
-def catalogo():
-    return render_template('catalogo.html')
+    # Inicializa SQLAlchemy con la app
+    db.init_app(app)
 
+    # Crea las tablas si no existen
+    with app.app_context():
+        db.create_all()
+        seed_data()
+
+    # RUTAS
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/catalogo')
+    def catalogo():
+        return render_template('catalogo.html')
+
+    return app
+
+# Ejecuta la app
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
