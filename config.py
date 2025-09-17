288c2b6427dd0fb38ad6ee1ca9c62001c5fae31e
Adrihan249 <adrihanmendoza2018@gmail.com>
2025-09-16 20:28:32 -0500

    Enbelleciendo la Pagina

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 #config.py
 
-DB_URI = 'mysql+mysqlconnector://root:root@localhost/largophone'
+DB_URI = "mysql+mysqlconnector://root:ozuna1234%40@localhost:3306/largophone"
+
 
 class Config:
     SQLALCHEMY_DATABASE_URI = DB_URI
